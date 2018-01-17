50a5feeb2f72276d21e93c5c5464d8059085ed13
Veselin Dimitrov <vidimitrov94@gmail.com>
2018-01-18 00:08:59 +0200

    Solve the OddOccurrencesInArray challenge

---
+++ codility/odd-occurrences-in-array.js
@@ -0,0 +1,28 @@
+function solution(A) {
+    var last;
+    var paired = false;
+    var arr = A.sort(function(a, b) {
+        return a -b;
+    });
+
+    for(var i = 0; i < arr.length; i++){
+        if (i > 0) {
+            if (arr[i] != last) {
+                if (!paired) {
+                    return last;
+                } else {
+                    paired = false;
+                }
+            } else {
+                if (paired) {
+                    paired = false;
+                } else { 
+                    paired = true;
+                }
+            }
+        }
+        last = arr[i];
+    }
+    
+    return last;
+}
